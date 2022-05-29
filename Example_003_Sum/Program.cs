cc07fe81eb38edaaa1d6695d6b803e103c11209f
Helga <olyapepsycola@gmail.com>
2022-05-29 13:49:50 +0300

    Sum

---
+++ Example_003_Sum/Program.cs
@@ -0,0 +1,12 @@
+﻿int numberA = 5;
+int numberB = 11;
+
+Console.WriteLine (numberA + numberB);
+
+// or
+
+int numberC = 4;
+int numberD = 3;
+int result = numberC + numberD;
+
+Console.WriteLine (result);
