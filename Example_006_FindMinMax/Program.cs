c118c3778ac0c8d852b1cb1ec1deff8d67ddc0ab
Helga <olyapepsycola@gmail.com>
2022-05-29 15:28:02 +0300

    Add FindMax

---
+++ Example_006_FindMinMax/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 5;
+int b = 3;
+int c = 11;
+int d = 1;
+int e = 7;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write ("Максимальное число: ");
+Console.WriteLine (max);
