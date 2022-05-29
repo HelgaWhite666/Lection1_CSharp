a82888450373d45b28f9e156b543c33eda0b7032
Helga <olyapepsycola@gmail.com>
2022-05-29 15:18:28 +0300

    Add ToLower

---
+++ Example_005_SpecialUserName/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine ("Представьтесь, пожалуйста!");
+string userName = Console.ReadLine();
+
+if (userName.ToLower() == "ольга") {
+
+    Console.WriteLine ("Ну привет, солнышко :)");
+
+} else {
+
+    Console.Write ("Здравствуйте, ");
+    Console.Write (userName);
+    Console.WriteLine ("!");
+
+}
