6069f7297768fd3578dd2ecc2d7028ff8c12867d
Helga <olyapepsycola@gmail.com>
2022-05-29 14:41:37 +0300

    Add random to sum

--- Example_003_Sum/Program.cs
+++ Example_003_Sum/Program.cs
@@ -10,3 +10,16 @@ int numberD = 3;
 int result = numberC + numberD;
 
 Console.WriteLine (result);
+
+//or
+
+int numberE = new Random().Next(1,10); // 1 2 3 ... 9
+Console.Write ("Первое число: ");
+Console.WriteLine (numberE);
+
+int numberF = new Random().Next(1,10);
+Console.Write ("Второе число: ");
+Console.WriteLine (numberF);
+
+Console.Write ("Сумма: ");
+Console.WriteLine (numberE + numberF);
